d4307bb738dc388212db989bf132e50056571bf5
Riley Slayden <slaydenriley@gmail.com>
2020-08-16 15:31:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
 function createEmployeeRecord(array) {
 
 };
